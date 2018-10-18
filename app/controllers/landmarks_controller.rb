b38688a0e2dad251190cc8e43d0182f0f16644b3
Ayana Bando <ayanaflax@yahoo.com>
2018-10-18 15:01:36 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,7 +15,10 @@ class LandmarksController < ApplicationController
     erb :"landmarks/show"
   end
   
-  
+    get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
   
   post '/landmarks' do
     @landmark= Landmark.create(params['landmark'])
