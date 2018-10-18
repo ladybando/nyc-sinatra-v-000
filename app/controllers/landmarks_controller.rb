f3388bff4b423facb1a4e82f54cd3ab8cd867fc2
Ayana Bando <ayanaflax@yahoo.com>
2018-10-18 14:58:44 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,7 +6,7 @@ class LandmarksController < ApplicationController
   end
   
   get '/landmarks/new' do
-    @landmark = Landmark.find_by(params[:name])
+    @landmark = Landmark.find_by(params[:id])
     erb :'/landmarks/new'
   end
   
