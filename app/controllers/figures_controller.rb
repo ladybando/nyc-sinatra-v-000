582d2e0e1caf5fedd016d3466f5ff65a308ea159
Ayana Bando <ayanaflax@yahoo.com>
2018-10-18 14:55:04 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -21,5 +21,16 @@ class FiguresController < ApplicationController
      @figure.save
     redirect to "/figures/show"
   end
-
+	  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
