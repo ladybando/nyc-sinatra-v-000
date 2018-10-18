7fc341e432171aa49dff08cfbce4e506d639e0fb
Ayana Bando <ayanaflax@yahoo.com>
2018-10-18 15:03:38 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,6 +3,7 @@ class LandmarksController < ApplicationController
   get '/landmarks' do
     @landmarks = Landmark.all
     @figures = Figure.all
+    erb :'/landmarks/index'
   end
   
   get '/landmarks/new' do
